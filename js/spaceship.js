2563e778aa2f25a760368bff0f1c5cfd0083887e
Magnús Skúlason <maggisk@gmail.com>
2016-01-28 22:34:19 +0000

    kill the snowman!

--- js/spaceship.js
+++ js/spaceship.js
@@ -1,6 +1,6 @@
 var SCREEN_WIDTH = 750;
 var SCREEN_HEIGHT = 400;
-var SHOT_DELAY = 100;
+var SHOT_DELAY = 150;
 var SHOT_SPEED = 500;
 var AIRPLANE_SPEED = 100;
 
@@ -23,9 +23,29 @@ function arrayRemove(array, item) {
   if (i > -1) array.splice(i, 1);
 }
 
+function within(thing, x, y) {
+  return x >= thing.x && x <= thing.x + thing.width && y >= thing.y && y <= thing.y + thing.height;
+}
+
+function hasCornerInside(a, b) {
+  return within(a, b.x, b.y) // top left within a
+      || within(a, b.x + b.width, b.y) // top right within a
+      || within(a, b.x, b.y + b.height) // bottom left within a
+      || within(a, b.x + b.width, b.y + b.height) // bottom right
+}
+
+function collides(a, b) {
+  return (hasCornerInside(a, b) || hasCornerInside(b, a))
+    && Math.sqrt(
+         Math.pow((a.x + a.width / 2) - (b.x + b.width / 2), 2),
+         Math.pow((a.y + a.height / 2) - (b.x + b.height / 2), 2)
+       ) < (a.width / 2 + a.height / 2) + (b.width / 2 + b.height / 2);
+}
+
 var IMAGES = {
   airplane: createImage('Air plane.png'),
   bigben: createImage('Big_ben.png'),
+  snowman: createImage('Snowman.png'),
 };
 
 function Airplane() {
@@ -73,17 +93,44 @@ Shot.prototype.draw = function(ctx) {
   ctx.drawImage(IMAGES.bigben, 0, 0, this.width, this.height);
 }
 
+Shot.prototype.collide = function(other, state) {
+  if (other instanceof Enemy) {
+    arrayRemove(state.enemies, other);
+  }
+}
+
+function Enemy(x, y) {
+  this.x = x;
+  this.y = y;
+  this.width = 40;
+  this.height = 40;
+}
+
+Enemy.prototype.update = function(state) {
+  // TODO
+}
+
+Enemy.prototype.draw = function(ctx) {
+  ctx.translate(this.x, this.y);
+  ctx.drawImage(IMAGES.snowman, 0, 0, this.width, this.height);
+}
+
 function init() {
   var state = {
     shots: [],
+    enemies: [],
     airplane: new Airplane(),
     keysDown: {},
     prevTick: timestamp(),
     currentTick: null,
   };
 
+  for (var i = 0; i < 20; i++) {
+    state.enemies.push(new Enemy(i * 50, 10))
+  }
+
   function getThings() {
-    return state.shots.concat(state.airplane);
+    return state.shots.concat(state.enemies).concat(state.airplane);
   }
 
   var canvas = document.getElementById('canvas');
@@ -97,6 +144,15 @@ function init() {
     getThings().forEach(function(thing) {
       if (thing.update) thing.update(state);
     });
+
+    var things = getThings();
+    for (var i = 0; i < things.length; i++) {
+      for (var j = i + 1; j < things.length; j++) {
+        if (collides(things[i], things[j])) {
+          things[i].collide(things[j], state);
+        }
+      }
+    }
   }
 
   function draw() {
@@ -123,7 +179,7 @@ function init() {
   gameLoop();
 
   document.addEventListener('keydown', function(e) {
-    console.log(e.keyCode);
+    // console.log(e.keyCode);
     state.keysDown[e.keyCode] = true;
   });
   document.addEventListener('keyup', function(e) {
